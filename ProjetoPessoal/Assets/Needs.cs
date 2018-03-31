d4ab6c268e73d27b75f9c836dac4a18faab6a24c
arnaldoeloi <arnaldoeloi2@gmail.com>
2018-03-31 03:25:48 -0300

    Barrinhas de Necessidades adicionadas e sincronizadas com UI

---
+++ ProjetoPessoal/Assets/Needs.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Security.Cryptography.X509Certificates;
+
+public class Needs : MonoBehaviour {
+
+	private float time = 0.0f;
+	
+
+	public float decayTime=0.1f;
+	public float hunger=50f;
+	public float tireness=50f;
+	public float boredom=50f;
+	public float social=50f;
+
+	public float minDecayLevelperTime = 0f;
+	public float maxDecayLevelperTime = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+
+		if (time >= decayTime) {
+			time = time - decayTime;
+			decayRandom();
+			Debug.Log ("Hunger: "+hunger);
+		}
+
+	}
+
+	void decayRandom(){
+		
+		hunger -= Random.Range (minDecayLevelperTime, maxDecayLevelperTime);
+		boredom -= Random.Range (minDecayLevelperTime, maxDecayLevelperTime);
+		social -= Random.Range (minDecayLevelperTime, maxDecayLevelperTime);
+		tireness -= Random.Range (minDecayLevelperTime, maxDecayLevelperTime);
+
+
+	}
+}
---
+++ ProjetoPessoal/Assets/SimsNeedsUI.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SimsNeedsUI : MonoBehaviour {
+
+	private Needs needs;
+	public Slider hungerBar;
+	public Slider boredomBar;
+	public Slider tirenessBar;
+	public Slider socialBar;
+
+
+	public GameObject simsCharacter;
+
+
+	// Use this for initialization
+	void Start () {
+		needs = (Needs) simsCharacter.GetComponent ("Needs");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		hungerBar.value = needs.hunger/100f;
+		boredomBar.value = needs.boredom/100f;
+		tirenessBar.value = needs.tireness/100f;
+		socialBar.value = needs.social/100f;
+
+		Debug.Log ("UI Boredom Value="+needs.boredom);
+		Debug.Log ("UI Social Value="+needs.social);
+		Debug.Log ("UI Tireness Value="+needs.tireness);
+		Debug.Log ("UI Hunger Value="+needs.hunger);
+	}
+}
