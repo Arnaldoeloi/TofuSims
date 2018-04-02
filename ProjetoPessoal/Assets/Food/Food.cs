06616314a8c049abfb2aece97fa1b83c5300ee15
arnaldoeloi <arnaldoeloi2@gmail.com>
2018-04-02 20:09:52 -0300

    Problema com os add resolvido, repositório funcional

---
+++ ProjetoPessoal/Assets/Food/Food.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Food : InteractiveElement {
+
+	private Renderer rend;
+
+	// Use this for initialization
+	void Start () {
+		rend = GetComponent<Renderer> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void execute(){
+		rend.enabled = false;
+		GameObject.DestroyObject(this);
+	}
+}
---
+++ ProjetoPessoal/Assets/FridgeElement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FridgeElement : InteractiveElement {
+
+	public Food[] foods = new Food[10];
+	public Food applePrefab;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public override void execute ()
+	{
+		Debug.Log (foods.Length);
+
+		foreach (Food food in foods) {
+			
+		}
+	}
+
+	public void addApple(){
+		//foods. = applePrefab;
+	}
+}
